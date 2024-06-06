83efa2f1a41c3aed1d5d6037177b0de452597c91
Alexandre Le Gal <alexandrelegal02@gmail.com>
2024-06-06 13:28:55 +0200

    added pre done work

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let retexps = document.getElementsByClassName("retexp");
+const scrolls = new Map();
+
+for (let retexp in retexps)
+    scrolls.set(retexp, false);
+
+const scroll = (retexp) =>{
+    if (scrolls.get(retexp) == false)
+        retexp.style.transform = "translate-y(0%)";
+    else
+        retexp.style.transform = "translate-y(100%)";
+    scrolls.set(retexp, !scrolls.get(retexp));
+}
+
+for (let i = 0; i<retexps.length; i++)
+    retexps[i].addEventListener("click", scroll);
